3d523363b1d76191d0bb2c77d823b9354fc58444
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-01-16 16:11:51 +0300

    Initial project setup

---
+++ README.md
@@ -0,0 +1,13 @@
+# rogalix
+
+Rogue-like game project for jvm-languages course at SPbAU 7th term
+
+## Running
+
+```diff
+- TODO: Add running info. 
+```
+
+## License
+
+[MIT](LICENSE)
---
+++ src/test/kotlin/ru/spbau/mit/test.kt
@@ -0,0 +1,11 @@
+package ru.spbau.mit
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+    @Test
+    fun testMain() {
+        assertEquals(true, true, "true is true")
+    }
+}
